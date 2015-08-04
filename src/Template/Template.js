db25e473617ebcf3c480ad8ef9237fb030ef9fe4
Daniel Freedman <dfreedm@google.com>
2015-08-03 18:10:03 -0700

    Fix breakages in IE and Safari 7

--- src/Template/Template.js
+++ src/Template/Template.js
@@ -14,6 +14,9 @@ if (typeof HTMLTemplateElement === 'undefined') {
 
     var TEMPLATE_TAG = 'template';
 
+    var contentDoc = document.implementation.createHTMLDocument('template');
+    var canDecorate = true;
+
     /**
       Provides a minimal shim for the <template> element.
     */
@@ -27,9 +30,36 @@ if (typeof HTMLTemplateElement === 'undefined') {
     HTMLTemplateElement.decorate = function(template) {
       if (!template.content) {
         template.content = template.ownerDocument.createDocumentFragment();
+      }
         var child;
         while (child = template.firstChild) {
           template.content.appendChild(child);
+        }
+      // add innerHTML to template, if possible
+      // Note: this throws on Safari 7
+      if (canDecorate) {
+        try {
+          Object.defineProperty(template, 'innerHTML', {
+            get: function() {
+              var o = '';
+              for (var e = this.content.firstChild; e; e = e.nextSibling) {
+                o += e.outerHTML || escapeData(e.data);
+              }
+              return o;
+            },
+            set: function(text) {
+              contentDoc.body.innerHTML = text;
+              while (this.content.firstChild) {
+                this.content.removeChild(this.content.firstChild);
+              }
+              while (contentDoc.body.firstChild) {
+                this.content.appendChild(contentDoc.body.firstChild);
+              }
+            },
+            configurable: true
+          });
+        } catch (err) {
+          canDecorate = false;
         }
       }
     };
@@ -46,9 +76,39 @@ if (typeof HTMLTemplateElement === 'undefined') {
     };
 
     // auto-bootstrapping for main document
-    addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('DOMContentLoaded', function() {
       HTMLTemplateElement.bootstrap(document);
     });
+
+    // Patch document.createElement to ensure newly created templates have content
+    var createElement = document.createElement;
+    document.createElement = function() {
+      'use strict';
+      var el = createElement.apply(document, arguments);
+      if (el.localName == 'template') {
+        HTMLTemplateElement.decorate(el);
+      }
+      return el;
+    };
+
+    var escapeDataRegExp = /[&\u00A0<>]/g;
+
+    function escapeReplace(c) {
+      switch (c) {
+        case '&':
+          return '&amp;';
+        case '<':
+          return '&lt;';
+        case '>':
+          return '&gt;';
+        case '\u00A0':
+          return '&nbsp;';
+      }
+    }
+
+    function escapeData(s) {
+      return s.replace(escapeDataRegExp, escapeReplace);
+    }
 
   })();
 }
